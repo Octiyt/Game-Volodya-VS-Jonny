a2ca78659c7a9457e1d7542c5e283fd58c2b94d0
deny <denn.chesnokov@gmail.com>
2024-12-22 12:25:14 +0200

    HOMEWORK

--- Assasin.py
+++ Assasin.py
@@ -2,7 +2,7 @@ import random
 from Lesson2.character import Character
 
 class Assassin(Character):
-    def take_damage(self, damage):
+    def attack(self, damage):
         if random.random() < 0.1:
             damage = 1000
             print(f"{self.name} завдав супер удар! Нанесено 1000 одиниць шкоди.")
